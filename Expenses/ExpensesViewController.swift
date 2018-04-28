5a505de680ac1bfc0c1d1f710f413ee55465f4bc
Joe Timlin <jhthx9@mail.missouri.edu>
2018-04-27 21:09:26 -0500

    Finished basic requirements

--- Expenses/ExpensesViewController.swift
+++ Expenses/ExpensesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ExpensesViewController: UIViewController {
     
@@ -14,11 +15,30 @@ class ExpensesViewController: UIViewController {
     
     let dateFormatter = DateFormatter()
     
+    var expenses = [Expense]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         dateFormatter.timeStyle = .long
         dateFormatter.dateStyle = .long
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        guard let appDelete = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelete.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Expense> = Expense.fetchRequest()
+        
+        do {
+            expenses = try managedContext.fetch(fetchRequest)
+            
+            expensesTableView.reloadData()
+        } catch {
+            print("Fetch could not be performed")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,22 +47,66 @@ class ExpensesViewController: UIViewController {
     }
     
     @IBAction func addNewExpense(_ sender: Any) {
-        
+        performSegue(withIdentifier: "showExpense", sender: self)
     }
 
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? SingleExpenseViewController,
+            let selectedRow = self.expensesTableView.indexPathForSelectedRow?.row else {
+                return
+        }
+        destination.existingExpense = expenses[selectedRow]
+    }
+    
+    func deleteExpense(at indexPath: IndexPath) {
+        let expense = expenses[indexPath.row]
+        
+        if let managedContext = expense.managedObjectContext {
+            managedContext.delete(expense)
+            
+            do {
+                try managedContext.save()
+                
+                self.expenses.remove(at: indexPath.row)
+                
+                expensesTableView.deleteRows(at: [indexPath], with: .automatic)
+            } catch {
+                print("Expense could not be deleted")
+                
+                expensesTableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        }
+    }
+    
 }
 
 extension ExpensesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return expenses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = expensesTableView.dequeueReusableCell(withIdentifier: "expenseCell", for: indexPath)
+        let expense = expenses[indexPath.row]
+        
+        cell.textLabel?.text = expense.name
+        
+        if let date = expense.date {
+            cell.detailTextLabel?.text = dateFormatter.string(from: date)
+        }
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            deleteExpense(at: indexPath)
+        }
+    }
 }
+
+
 
 extension ExpensesViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
--- Expenses/SingleExpenseViewController.swift
+++ Expenses/SingleExpenseViewController.swift
@@ -14,12 +14,25 @@ class SingleExpenseViewController: UIViewController {
     @IBOutlet weak var amountTextField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    var existingExpense: Expense?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nameTextField.delegate = self
         amountTextField.delegate = self
-
+        
+        nameTextField.text = existingExpense?.name
+        
+        if let amount = existingExpense?.amount {
+            amountTextField.text = "\(amount)"
+        }
+        
+        if let date = existingExpense?.date {
+            datePicker.date = date
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +41,34 @@ class SingleExpenseViewController: UIViewController {
     }
     
     @IBAction func saveExpense(_ sender: Any) {
+        let name = nameTextField.text
+        let amountText = amountTextField.text ?? ""
+        let amount = Double(amountText) ?? 0.0
+        let date = datePicker.date
+        
+        var expense: Expense?
+        
+        if let existingExpense = existingExpense {
+            existingExpense.name = name
+            existingExpense.amount = amount
+            existingExpense.date = date
+            
+            expense = existingExpense
+        } else {
+            expense = Expense(name: name, amount: amount, date: date)
+        }
+        
+        if let expense = expense {
+            do {
+                let managedContext = expense.managedObjectContext
+                
+                try managedContext?.save()
+                
+                self.navigationController?.popViewController(animated: true)
+            } catch {
+                print("Context could not be saved.")
+            }
+        }
         
     }
 }
